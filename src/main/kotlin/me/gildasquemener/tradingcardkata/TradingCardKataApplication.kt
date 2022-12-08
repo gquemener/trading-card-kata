5f8e556bb36e424fd6350e3bdc0970673adbb88f
Gildas Quéméner <gildas.quemener@codurance.com>
2022-12-08 09:19:54 +0100

    Initial commit

---
+++ src/main/kotlin/me/gildasquemener/tradingcardkata/TradingCardKataApplication.kt
@@ -0,0 +1,11 @@
+package me.gildasquemener.tradingcardkata
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TradingCardKataApplication
+
+fun main(args: Array<String>) {
+    runApplication<TradingCardKataApplication>(*args)
+}
---
+++ src/test/kotlin/me/gildasquemener/tradingcardkata/TradingCardKataApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.gildasquemener.tradingcardkata
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TradingCardKataApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
